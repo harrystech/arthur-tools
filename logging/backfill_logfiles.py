83230276af7d875ec5e6f7909a6b93fcce2c9922
Tom Vogels <thomas.vogels@harrys.com>
2020-02-15 20:29:19 -0500

    Add log results (and format with black)

--- logging/backfill_logfiles.py
+++ logging/backfill_logfiles.py
@@ -7,6 +7,7 @@ This will only load files from stderr (which Arthur uses for its log output).
 """
 
 import argparse
+import base64
 import json
 import logging
 import urllib.parse
@@ -48,19 +49,12 @@ def invoke_log_parser(function_name, bucket_name, object_key):
     """
     logging.info("Invoking log parser for s3://{}/{}".format(bucket_name, object_key))
     payload = {
-        'Records': [
+        "Records": [
             {
                 "eventTime": datetime.utcnow().isoformat(),
                 "eventName": "ObjectCreated:Put",
                 "eventSource": "aws:s3",
-                "s3": {
-                    "bucket": {
-                        "name": bucket_name
-                    },
-                    "object": {
-                        "key": urllib.parse.quote_plus(object_key)
-                    }
-                }
+                "s3": {"bucket": {"name": bucket_name}, "object": {"key": urllib.parse.quote_plus(object_key)}},
             }
         ]
     }
@@ -69,49 +63,51 @@ def invoke_log_parser(function_name, bucket_name, object_key):
 
     try:
         response = client.invoke(
-            FunctionName=function_name,
-            InvocationType='RequestResponse',
-            LogType='Tail',
-            Payload=payload_bytes,
+            FunctionName=function_name, InvocationType="RequestResponse", LogType="Tail", Payload=payload_bytes,
         )
     except Exception:
         logging.exception("Failed to upload 's3://{}/{}':".format(bucket_name, object_key))
         raise
+    logging.info("LogResult={!s}".format(base64.standard_b64decode(response["LogResult"])))
 
-    status_code = response['StatusCode']
+    status_code = response["StatusCode"]
     if status_code == 200:
         logging.info("Finished parsing of 's3://{}/{}' successfully".format(bucket_name, object_key))
     else:
-        logging.warning("Finished parsing of 's3://{}/{}' with status code {}".format(bucket_name, object_key,
-                                                                                      status_code))
+        logging.warning(
+            "Finished parsing of 's3://{}/{}' with status code {}".format(bucket_name, object_key, status_code)
+        )
 
 
 def main(bucket, prefix, function_name, days_in_past_limit, threads):
     logging.info("Attempting to load log files from s3://{}/{} using {}".format(bucket, prefix, function_name))
     logging.info("Arguments that limit objects: prefix={}, days_limit={}".format(prefix, days_in_past_limit))
     objects = list_objects(bucket, prefix, days_in_past_limit)
-    object_keys = map(itemgetter(0), sorted(objects, key=itemgetter(1), reverse=True))
+    object_keys = list(map(itemgetter(0), sorted(objects, key=itemgetter(1), reverse=True)))
     logging.info("Found {} object(s) to process".format(len(object_keys)))
 
     logging.info("Starting thread pool (with {} threads".format(threads))
     lambda_caller = partial(invoke_log_parser, function_name, bucket)
-    with ThreadPoolExecutor(max_workers=threads, thread_name_prefix='lambda_caller') as executor:
+    with ThreadPoolExecutor(max_workers=threads, thread_name_prefix="lambda_caller") as executor:
         executor.map(lambda_caller, object_keys)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format="%(asctime)s %(levelname)s %(name)s (%(threadName)s) %(message)s")
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s (%(threadName)s) %(message)s")
 
     parser = argparse.ArgumentParser(
         description="Backfill Elasticsearch cluster using log files from the given bucket using the given lambda."
     )
-    parser.add_argument("--prefix",
-                        default="_logs",
-                        help="Limit objects to those with the given prefix (default: %(default)s)")
-    parser.add_argument("--days-limit", type=int,
-                        default=365, metavar="N",
-                        help="Limit objects to those modified within the last N days (default: %(default)s)")
+    parser.add_argument(
+        "--prefix", default="_logs", help="Limit objects to those with the given prefix (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--days-limit",
+        type=int,
+        default=365,
+        metavar="N",
+        help="Limit objects to those modified within the last N days (default: %(default)s)",
+    )
     parser.add_argument("--threads", type=int, default=10, help="Number of threads to use")
     parser.add_argument("bucket", help="Name of bucket in S3")
     parser.add_argument("lambda_arn", help="ARN of Lambda that can process log files")
