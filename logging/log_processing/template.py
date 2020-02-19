9683127daab22561e4da486d3ce86086f4a46e95
Tom Vogels <thomas.vogels@harrys.com>
2020-02-19 07:44:34 -0500

    Apply black and isort to log_processing

--- logging/log_processing/template.py
+++ logging/log_processing/template.py
@@ -1,23 +1,16 @@
 LOG_RECORD_PROPERTIES = {
     "application_name": {"type": "keyword"},
     "environment": {"type": "keyword"},
-    "logfile": {
-        "type": "keyword",
-    },
+    "logfile": {"type": "keyword"},
     "data_pipeline": {
         "properties": {
             "id": {"type": "keyword"},
             "component": {"type": "keyword"},
             "instance": {"type": "keyword"},
-            "attempt": {"type": "keyword"}
+            "attempt": {"type": "keyword"},
         }
     },
-    "emr_cluster": {
-        "properties": {
-            "id": {"type": "keyword"},
-            "step_id": {"type": "keyword"}
-        }
-    },
+    "emr_cluster": {"properties": {"id": {"type": "keyword"}, "step_id": {"type": "keyword"}}},
     "@timestamp": {"type": "date", "format": "strict_date_optional_time"},  # generic ISO datetime parser
     "datetime": {
         "properties": {
@@ -29,39 +22,18 @@ LOG_RECORD_PROPERTIES = {
             "day_of_week": {"type": "integer"},
             "hour": {"type": "integer"},
             "minute": {"type": "integer"},
-            "second": {"type": "integer"}
+            "second": {"type": "integer"},
         },
     },
     "etl_id": {"type": "keyword"},
     "log_level": {"type": "keyword"},
-    "logger": {
-        "type": "text",
-        "analyzer": "simple",
-        "fields": {
-            "name": {
-                "type": "keyword"
-            }
-        }
-    },
+    "logger": {"type": "text", "analyzer": "simple", "fields": {"name": {"type": "keyword"}}},
     "thread_name": {"type": "keyword"},
-    "source_code": {
-        "properties": {
-            "filename": {"type": "text"},
-            "line_number": {"type": "integer"},
-        }
-    },
+    "source_code": {"properties": {"filename": {"type": "text"}, "line_number": {"type": "integer"}}},
     "message": {
         "type": "text",
         "analyzer": "standard",
-        "fields": {
-            "raw": {
-                "type": "keyword"
-            },
-            "english": {
-                "type": "text",
-                "analyzer": "english"
-            }
-        }
+        "fields": {"raw": {"type": "keyword"}, "english": {"type": "text", "analyzer": "english"}},
     },
     "monitor": {
         "properties": {
@@ -71,25 +43,13 @@ LOG_RECORD_PROPERTIES = {
             "target": {"type": "keyword"},
             "elapsed": {"type": "float"},
             "rowcount": {"type": "long"},
-            "error_codes": {"type": "text"}
+            "error_codes": {"type": "text"},
         }
     },
-    "parser": {
-        "properties": {
-            "start_pos": {"type": "long"},
-            "end_pos": {"type": "long"},
-            "chars": {"type": "long"}
-        },
-    },
+    "parser": {"properties": {"start_pos": {"type": "long"}, "end_pos": {"type": "long"}, "chars": {"type": "long"}}},
     # These last properties are only used by the Lambda handler:
     "lambda_name": {"type": "keyword"},
     "lambda_version": {"type": "keyword"},
-    "context": {
-        "properties": {
-            "remaining_time_in_millis": {"type": "long"}
-        }
-    },
-    "original_logfile": {
-        "type": "keyword",
-    }
+    "context": {"properties": {"remaining_time_in_millis": {"type": "long"}}},
+    "original_logfile": {"type": "keyword"},
 }
