17c4dd1d1f17b2c342e406c42cfa123097ba4819
Tom Vogels <thomas.vogels@harrys.com>
2020-08-21 09:32:21 -0400

    Parse and index control message REPORT

--- cloudwatch_logs/cw_logs_to_es/cw_log_parser.py
+++ cloudwatch_logs/cw_logs_to_es/cw_log_parser.py
@@ -1,75 +1,100 @@
 import json
 import logging
-from datetime import datetime, timezone
-from typing import List
+from datetime import datetime
+from typing import Iterator
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
 class CloudWatchLogsParser:
-    def parse_log_events(self, log_data: dict) -> List[dict]:
-        bulk_payload = []
-        log_group_name = (log_data["logGroup"].split("/")[-1]).lower()
+    def parse_log_events(self, log_data: dict) -> Iterator[dict]:
+        shared = {
+            "aws_account": log_data["owner"],
+            "log_group": log_data["logGroup"],
+            "log_stream": log_data["logStream"],
+        }
 
         for event in log_data["logEvents"]:
-            logger.debug(f"Decoded: {event}")
-            msg_str = event["message"]
-            # delete bane of logs  since we already captured it as msg_str
-            del event["message"]
-            # for now we'll discard aws lambda usage logs
-            if (
-                msg_str.startswith("START Requ")
-                or msg_str.startswith("END Requ")
-                or msg_str.startswith("REPORT Requ")
-            ):
+            logger.debug(f"Parsing decoded log event id={event['id']}:\n{event}")
+
+            timestamp = datetime.fromtimestamp(event["timestamp"] / 1000.0)
+            index_name = f"cw-logs-{timestamp.strftime('%Y-%m')}"
+
+            message = event["message"]
+            if message.startswith(("START RequestId", "END RequestId")):
                 continue
+            if message.startswith("REPORT RequestId"):
+                payload = self._parse_control_message(message)
+            else:
+                payload = self._parse_single_event(message)
 
-            source = {}
-            source["@timestamp"] = datetime.fromtimestamp(event["timestamp"] / 1000.0)
-            source["@aws_account"] = (log_data["owner"]).lower()
-            source["@log_group"] = (log_data["logGroup"]).lower()
-            source["@log_stream"] = (log_data["logStream"]).lower()
-            source["@payload"] = msg_str
-            source.update(event)
-            try:
-                source.update(json.loads(msg_str))
-            except Exception:
-                source.update(self.parse_dirty_json(msg_str))
+            source = dict(shared)
+            source.update(payload)
+            source["@timestamp"] = timestamp
 
-            index_name = f"cw-{log_group_name}-{datetime.now(timezone.utc).strftime('%Y-%m-%d')}"
-            index_name = index_name.replace(":", "").lower()
-
+            # Build an action as required for bulk updates in ES.
             action = {
-                "_index": index_name,
-                "_type": f"cw-logs-{log_group_name}",
                 "_id": event["id"],
+                "_index": index_name,
+                "_op_type": "index",
                 "_source": source,
             }
+            logger.debug(f"Adding action:\n{json.dumps(action, default=str)}")
+            yield action
 
-            bulk_payload.append(action)
+    @classmethod
+    def _parse_control_message(cls, control: str) -> dict:
+        clean_control = control.strip()
+        fields = clean_control.split("\t")
+        report = dict(message=clean_control, log_type="REPORT")
+        for key, value in [field.split(":") for field in fields]:
+            if key == "REPORT RequestId":
+                report["aws_request_id"] = value.strip()
+            elif key == "Duration":
+                report["duration_in_ms"] = float(value[1:-3])
+            elif key == "Billed Duration":
+                report["billed_duration_in_ms"] = float(value[1:-3])
+            elif key == "Memory Size":
+                report["memory_size_in_mb"] = int(value[1:-3])
+            elif key == "Max Memory Used":
+                report["max_memory_used_in_mb"] = int(value[1:-3])
+            elif key == "Init Duration":
+                report["init_duration_in_ms"] = float(value[1:-3])
+        return report
 
-        logger.debug(f"Inspect bulk payload:\n{json.dumps(bulk_payload, default=str)}")
-        return bulk_payload
+    @classmethod
+    def _parse_single_event(cls, json_str: str) -> dict:
+        try:
+            return json.loads(json_str)
+        except Exception as exc:
+            # Parsing as JSON failed, try to recover by looking for pairs.
+            recovered = cls._parse_dirty_json(json_str)
+            if not recovered:
+                # OK, this doesn't look like JSON at all.
+                return dict(message=json_str, cwl_parser_exception=repr(exc))
+            return dict(recovered, cwl_parser_exception=repr(exc))
 
-    def parse_dirty_json(self, dirty_json_str: str) -> dict:
-        parsed_json = {}
-        payload_str: str = dirty_json_str.replace("{", ", ").replace("}", ", ").replace("\n", "")
+    @classmethod
+    def _parse_dirty_json(cls, dirty_json_str: str) -> dict:
         payload_str = (
-            payload_str.replace("\\", "").replace('"', "").replace("\\s", "").replace("\n", "")
+            dirty_json_str.replace("{", ", ")
+            .replace("}", ", ")
+            .replace("\n", "")
+            .replace("\\", "")
+            .replace('"', "")
+            .replace("\\s", "")
         )
-        logger.debug(f"PAYLOAD STR: {payload_str}")
         pairs = payload_str.split(", ")
+        logger.debug(f"Recovered payload: {pairs}")
 
-        for p in pairs:
-            logger.debug(f"PAIR: {p}")
-            arr = p.split(": ")
-            if len(arr) == 0 or len(arr) == 1 or arr[0] is None or len(arr[0]) == 0:
-                logger.debug(f"WILL DISCARD: {arr}")
-                continue
+        parsed_json = {}
+        for pair in pairs:
+            arr = pair.split(": ")
+            if len(arr) < 2 or not arr[0]:
+                logger.debug(f"Discarding non-pair: {pair}")
             elif arr[0] == "timestamp":
                 parsed_json["log_record_timestamp"] = arr[1]
-                continue
             elif arr[0] == "message":  # don't key contents of payload
                 break
             else:
